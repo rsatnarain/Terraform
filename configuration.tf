0124347e0433c8824c1335bed8a0533586cd1f34
rsatnarain <type.unique_0o@icloud.com>
2026-04-14 19:46:38 -0400

    Add AWS provider configuration and workspace settings

---
+++ configuration.tf
@@ -0,0 +1,5 @@
+#configure the provider
+provider "aws" {
+    region = "us-east-1"
+}
+
--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-provider "aws" 
-{
+# Configure the AWS provider
+provider "aws" {
     region = "us-east-1"
 }
+
+# Resources
+
+
+# Variables
+
+
+# Outputs
